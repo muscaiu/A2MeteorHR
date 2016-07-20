eaa19cfaa78c6e15209914b3135be15e9e40350c
Laptop <Laptop>
2016-07-20 17:35:08 +0300

    Imported router

--- client/app.ts
+++ client/app.ts
@@ -4,15 +4,16 @@ import 'reflect-metadata';
 import { Component } from '@angular/core';
 // REPLACED BY THE BELLOW ONE import { bootstrap } from '@angular/platform-browser-dynamic';
 import { bootstrap } from 'angular2-meteor-auto-bootstrap';
+import { provideRouter, ROUTER_DIRECTIVES, RouterConfig } from '@angular/router';
+
 import { Users } from '../collections/users.ts';
 import { UsersForm } from './imports/users-form/users-form'
-
 import template from './app.html';
 
 @Component({
   selector: 'app',
   template,
-  directives: [UsersForm]
+  directives: [UsersForm, ROUTER_DIRECTIVES]
 })
 
 class a2meteorhr {
