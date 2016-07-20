969d80d00ac1ba82a6209c84c3e20628f03da9a4
Laptop <Laptop>
2016-07-20 16:13:18 +0300

    Displaying users from db.users

--- client/app.ts
+++ client/app.ts
@@ -4,6 +4,7 @@ import 'reflect-metadata';
 import { Component } from '@angular/core';
 // REPLACED BY THE BELLOW ONE import { bootstrap } from '@angular/platform-browser-dynamic';
 import { bootstrap } from 'angular2-meteor-auto-bootstrap';
+import { Users } from '../collections/users.ts';
 
 import template from './app.html';
 
@@ -12,20 +13,10 @@ import template from './app.html';
   template
 })
 class a2meteorhr {
-    users : Array<Object>;
+    users : Mongo.Cursor<Object>;
 
     constructor(){
-        this.users = [
-            {name: 'test1',
-             email: 'test1@test.test'
-            },
-            {name: 'test2',
-             email: 'test2@test.test'
-            },
-            {name: 'test3',
-             email: 'test3@test.test'
-            },
-        ]
+        this.users = Users.find();
     }
  }
 
